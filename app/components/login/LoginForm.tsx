d180854c4c9ca26b157e2206874cc267e40c84ee
zahra-hsb <zahra16toa@gmail.com>
2025-10-22 11:18:25 -0700

    fix: add react hook form to check the data of  form

--- app/components/login/LoginForm.tsx
+++ app/components/login/LoginForm.tsx
@@ -6,13 +6,15 @@ import Stack from '@mui/material/Stack';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { loginServerAction } from '~/serverActions/loginActions/loginSeverAction';
 import { LoginSchema } from '~/schemas/loginSchema';
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const LoginForm = () => {
 
-    // const { data } = useQuery({
-    //     queryKey: ['login'],
-    //     queryFn: (data) => loginServerAction(data)
-    // })
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(LoginSchema)
+    })
+
     const mutation = useMutation({
         mutationFn: loginServerAction,
         // TODO add dialogue or toast to show the success message
@@ -22,25 +24,28 @@ const LoginForm = () => {
     });
 
     return (
-        <>
-            <TextField fullWidth id="outlined-required" required label='username' type='text' placeholder='username' name='username' className='' variant='standard' />
-            <TextField fullWidth id="outlined-required" required label='password' type='password' placeholder='password' className='' name='password' variant='standard' />
+        <form onSubmit={handleSubmit((data) => mutation.mutate(data))} className='flex flex-col items-center w-full gap-5'>
+            <div className='w-full'>
+                <TextField {...register("username")} fullWidth required label='username' type='text' placeholder='username' error={errors.username ? true : false} name='username' id="" className='' variant='standard' />
+                <span className='w-full text-xs text-left text-red-400'>{errors.username ? errors.username.message : ""}</span>
+            </div>
+            <div className='w-full'>
+                <TextField {...register("password")} error={errors.password ? true : false} fullWidth required label='password' type='password' placeholder='password' className='' name='password' id="" variant='standard' />
+                <span className='w-full text-xs text-left text-red-400'>{errors.password ? errors.password.message : ""}</span>
+            </div>
             <Stack sx={{
                 flexDirection: {
                     lg: 'row'
                 }
             }} width={'100%'} flex={'flex'} gap={1}>
-                <Button onClick={() => {
-                    mutation.mutate({})
-                }} fullWidth loadingIndicator="loading" variant='contained' className='' type='button' size='medium' startIcon={<LoginIcon />}>
+                <Button fullWidth loadingIndicator="loading" variant='contained' className='' type='submit' size='medium' startIcon={<LoginIcon />}>
                     Login
                 </Button>
                 <Button fullWidth variant="outlined" startIcon={<RegisterIcon />}>
                     Register
                 </Button>
-                {/* {mutation.data} */}
             </Stack>
-        </>
+        </form>
     )
 }
 

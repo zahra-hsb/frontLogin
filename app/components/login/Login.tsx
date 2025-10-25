b083ed8cb69840d7d2a48badbc685c5991222c1c
zahra-hsb <zahra16toa@gmail.com>
2025-10-25 01:44:28 -0700

    add max width and max height to make the login form responsive

--- app/components/login/Login.tsx
+++ app/components/login/Login.tsx
@@ -18,7 +18,7 @@ const Login = () => {
                 xs: 2
             },
         }}
-            className='w-screen h-screen'
+            className='w-screen h-screen max-w-7xl mx-auto'
         >
             <Stack
                 padding={5}
@@ -31,7 +31,7 @@ const Login = () => {
                         xl: 0,
                     }
                 }}
-                className='xl:h-full border border-gray-300 dark:border-[#555] hover:shadow duration-300 justify-between dark:hover:border! dark:hover:border-[#999]! bg-linear-to-bl dark:from-[#1f1f1f] dark:to-[#171717] from-[#fdfdfd]'
+                className='xl:h-full border border-gray-300 dark:border-[#555] hover:shadow max-h-96 duration-300 justify-between dark:hover:border! dark:hover:border-[#999]! bg-linear-to-bl dark:from-[#1f1f1f] dark:to-[#171717] from-[#fdfdfd]'
                 flexDirection={'column'} spacing={3} justifyItems={'flex-end'}  alignItems={'center'}>
                 <h3 className='text-3xl'>
                     Login
